e09cd23ae88a3c0188e49f72dce6b7302d7fae5c
Prince Kumar <34896515+prince2000kr@users.noreply.github.com>
2020-09-05 08:42:00 +0530

    CLickable button added

--- popup.ts
+++ popup.ts
@@ -4,7 +4,7 @@ let recButton = document.getElementById('recButton');
 //     changeColor.style.backgroundColor = data.color;
 //     changeColor.setAttribute('value', data.color);
 // });
-
+let isClicked = false;
 recButton.onclick = function (element) {
     // let color = element.target.value;
     // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -12,11 +12,19 @@ recButton.onclick = function (element) {
     //         tabs[0].id,
     //         { code: 'document.body.style.backgroundColor = "' + color + '";' });
     // });
+    isClicked = !isClicked;
+    if (isClicked) {
+        chrome.storage.sync.get('color', function (data) {
+            recButton.style.backgroundColor = data.color;
+            recButton.setAttribute('value', data.color);
+        });
 
-    chrome.storage.sync.get('color', function (data) {
-        recButton.style.backgroundColor = data.color;
-        recButton.setAttribute('value', data.color);
-    });
+        //start recording
+        
+    }
+    else {
+        recButton.style.backgroundColor = null;
+    }
 
 };
 
